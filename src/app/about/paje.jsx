28f0f83fa56623108d4eb230e39aff957e7bdbd0
fearlessqueenwhite <fearlessqueenwhite@gmail.com>
2024-02-23 11:51:25 +0100

    my first commit

---
+++ src/app/about/paje.jsx
@@ -0,0 +1,14 @@
+import styles from './page.module.css'
+
+export default function About(){
+
+    return(
+        // if we are inside the return and we use {}, it indicates
+        // that we want to perform a javascript
+        <>
+        <h1 className="">This is the about page</h1>
+        <p className={`$P`}></p>
+        </>
+        
+    )
+}
